10712e64361984ac5c9b988da3f2f0bf9cba48e7
julietaRecoba <recobajulieta@gmail.com>
2024-07-02 14:20:29 -0300

    IMG ERROR

--- JS/index.js
+++ JS/index.js
@@ -5,14 +5,14 @@ let contentToAppend = "";
 function carouselClientes() {
     for (let i = 1; i < 19; i++) {
         if (i<10) {
-            contentToAppend = `<img src="imgs/clientes/download (${i}).png" class="clientes img-fluid mx-2" >`
+            contentToAppend = `<img src="Imgs/clientes/download (${i}).png" class="clientes img-fluid mx-2" >`
            let div= document.createElement("div");
             div.innerHTML = contentToAppend;
             div.setAttribute("id", "slide1_id" + i);
             carouselitem1.appendChild(div);
              
         } else {
-            contentToAppend = `<img src="imgs/clientes/download (${i}).png" class="clientes img-fluid mx-2" >`
+            contentToAppend = `<img src="Imgs/clientes/download (${i}).png" class="clientes img-fluid mx-2" >`
            let div= document.createElement("div");
             div.innerHTML = contentToAppend;
             div.setAttribute("id", "slide2_id" + i);
